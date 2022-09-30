dccc5482b84b617a7a9face133ccaeb5ded84c11
lora.duvnjak <lora.duvnjak@gmail.com>
2022-09-30 10:07:29 +0200

    Made user service

---
+++ src/main/java/hr/inovatrend/petManager/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package hr.inovatrend.petManager.Service.Impl;
+
+import hr.inovatrend.petManager.Entities.User;
+import hr.inovatrend.petManager.Repository.UserRepository;
+import hr.inovatrend.petManager.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+}
