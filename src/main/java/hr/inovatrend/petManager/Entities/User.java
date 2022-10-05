8dce16652c67e58dd3fd67366ff8cee368672b94
lora.duvnjak <lora.duvnjak@gmail.com>
2022-10-05 15:50:44 +0200

    Added OneToMany relation in User

--- src/main/java/hr/inovatrend/petManager/Entities/User.java
+++ src/main/java/hr/inovatrend/petManager/Entities/User.java
@@ -3,6 +3,8 @@ package hr.inovatrend.petManager.Entities;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -15,6 +17,9 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+
+    @OneToMany (mappedBy = "user")
+    List<Animal> animals = new ArrayList<>();
 
     private String name;
     private String surname;
