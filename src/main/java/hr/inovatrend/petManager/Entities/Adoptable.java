74461af171df808b0a10951d850f4104d71d4f2e
lora.duvnjak <lora.duvnjak@gmail.com>
2022-10-06 10:33:56 +0200

    Added @DateTimeFormat for Date variable

--- src/main/java/hr/inovatrend/petManager/Entities/Adoptable.java
+++ src/main/java/hr/inovatrend/petManager/Entities/Adoptable.java
@@ -3,6 +3,7 @@ package hr.inovatrend.petManager.Entities;
 import com.fasterxml.jackson.databind.DatabindException;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Embeddable;
 import java.util.Date;
@@ -12,7 +13,11 @@ import java.util.Date;
 @Embeddable
 public class Adoptable {
 
-    private Boolean adoptable;
+
+
+
+    private Boolean isAdoptable;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date adoptionDate;
     private String info; //info about previous owners/breeders or where the animal was found if it's stray
 
