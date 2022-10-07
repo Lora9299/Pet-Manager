f53dd6cfa4c3be1cb9c535c992f42bce90a9ccd5
lora.duvnjak <lora.duvnjak@gmail.com>
2022-10-07 13:00:13 +0200

    Made mappings for edit and info template

--- src/main/java/hr/inovatrend/petManager/Controller/AnimalController.java
+++ src/main/java/hr/inovatrend/petManager/Controller/AnimalController.java
@@ -18,7 +18,7 @@ public class AnimalController {
     private final AnimalService animalService;
 
     @GetMapping("/add")
-    private String saveAnimal(Model model){
+    private String saveAnimal(Model model) {
 
         model.addAttribute("animal", new Animal());
         model.addAttribute("types", AnimalType.values());
@@ -27,7 +27,7 @@ public class AnimalController {
     }
 
     @PostMapping("/add")
-    private String createAnimal(@ModelAttribute Animal animal){
+    private String createAnimal(@ModelAttribute Animal animal) {
 
         animalService.saveAnimal(animal);
         return "redirect:/animal/all-animals";
@@ -35,14 +35,44 @@ public class AnimalController {
 
 
     @GetMapping("/all")
-    private String allAnimals(Model model){
+    private String allAnimals(Model model) {
 
-    model.addAttribute("animals", animalService.getAll());
+        model.addAttribute("animals", animalService.getAll());
 
 
-    return "/animal/all-animals";
+        return "/animal/all-animals";
 
     }
 
+    @GetMapping("/info/{id}")
+    private String infoAnimal(@PathVariable("id") Long id, Model model) {
+
+        Animal animal = animalService.getAnimalById(id);
+
+        model.addAttribute("animals", animal);
+
+
+        return "/animal/info-animal";
+    }
+
+    @GetMapping("edit/{id}")
+    private String editAnimal(@PathVariable("id") Long id, Model model) {
+
+        Animal animal = animalService.getAnimalById(id);
+        model.addAttribute("animal", animal);
+        model.addAttribute("types", AnimalType.values());
+
+
+        return "/animal/edit-animal";
+
+    }
+
+    @PostMapping("edit/{id}")
+    private String animalEdit(@PathVariable("id") Long id, @ModelAttribute Animal animal){
+
+        animalService.saveAnimal(animal);
+
+        return "redirect:/animal/info/{id}";
+    }
 
 }
