d2ebd3118d467129d4adbc8f44ff9ffa47444904
lora.duvnjak <lora.duvnjak@gmail.com>
2022-10-07 09:52:02 +0200

    Removed spaces

--- src/main/java/hr/inovatrend/petManager/Entities/Adoptable.java
+++ src/main/java/hr/inovatrend/petManager/Entities/Adoptable.java
@@ -1,6 +1,5 @@
 package hr.inovatrend.petManager.Entities;
 
-import com.fasterxml.jackson.databind.DatabindException;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -12,9 +11,6 @@ import java.util.Date;
 @Setter
 @Embeddable
 public class Adoptable {
-
-
-
 
     private Boolean isAdoptable;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
